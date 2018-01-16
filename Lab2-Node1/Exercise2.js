cd1ba88563c70b2bcef6a4c4689a8a63ee3440b7
Thien An <986124@cs.mum.edu>
2018-01-16 15:11:02 -0600

    homework for lecture 2

---
+++ Lab2-Node1/Exercise2.js
@@ -0,0 +1,37 @@
+const tree = { 
+	name : "home", 
+	files : ["notes.txt","todo.txt"], 
+	subFolders: [	
+		{ name : "payroll", 
+		  files : ["paper.pdf","funds.csv"], 
+		  subFolders: [] 
+		}, 
+		{ name: "misc", 
+		  files : ["summer1.jpg","summer2.jpg", "summer3.jpg"], 
+		  subFolders: [
+			{ name : "logs", 
+			  files : ["logs1","logs2","logs3","logs4"], 
+			  subFolders: [] 
+		  }] 
+	}] 
+}; 
+
+var find = file => {
+    var findInTree = function (tree) {
+        if (tree.files != null) {
+            for (const index in tree.files) {
+                if (tree.files[index] === file) return true;
+            }
+        }
+        if (tree.subFolders != null) {
+            for (const folder of tree.subFolders) {
+                if (findInTree(folder)) return true;
+            }
+        }
+        return false;
+    }
+    return findInTree;
+};
+
+console.log(find("paper.pdf")(tree)); // true 
+console.log(find("randomfile")(tree)); // false
