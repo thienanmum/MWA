10b70c72ae997e782abb9fd70855983d599ffd39
annguyen <tanguyen@mum.edu>
2018-01-20 22:06:14 -0600

    configure validator globally

--- Lab6-RestAPI/app.js
+++ Lab6-RestAPI/app.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(validator());
 app.use(morgan('combined'));
 
 function validate(req) {
@@ -22,13 +23,13 @@ function validate(req) {
 
 app.route('/grades')
 .get((req, res) => res.status(200).json(grades.getAll()))
-.post(validator(), (req, res) => {
+.post((req, res) => {
     validate(req);
     res.status(200).json(grades.save(req.body));});
 
 app.route('/grades/:id')
 .get((req, res) => res.status(200).json(grades.get(parseInt(req.params.id))))
-.put(validator(), (req, res) => {
+.put((req, res) => {
     validate(req);
     req.body.id = parseInt(req.params.id);
     res.status(200).json(grades.save(req.body));
