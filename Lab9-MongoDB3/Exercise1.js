f4cab0290a47e2b4000a63579b310aac55748294
Thien An <986124@cs.mum.edu>
2018-01-24 14:14:23 -0600

    add Lecture9 exercise

---
+++ Lab9-MongoDB3/Exercise1.js
@@ -0,0 +1,37 @@
+Question_1
+db.zips.aggregate([
+    {$match:{state:'IA'}},
+    {$project: {_id:1}}
+])
+
+Question_2
+db.zips.aggregate([
+    {$match:{pop:{$lt: 1000}}},
+    {$project: {_id:1}}
+])
+
+Question_3
+db.zips.aggregate([
+    {$group: {
+        _id: {state:'$state', city:'$city'},
+        cnt: {$sum: 1}
+    }},
+    {$match: {cnt: {$gt: 1}}},
+    {$project: {state:'$_id.state', city:'$_id.city', numZips:'$cnt', _id:0}},
+    {$sort: {state:1, city:1}}
+])
+
+Question_4
+db.zips.aggregate([
+    {$group: {
+        _id: {state:'$state', city:'$city'},
+        population: {$sum: '$pop'}
+    }},
+    {$sort: {'_id.state':1, population:1}},
+    {$group: {
+        _id:'$_id.state',
+        city:{$first:'$_id.city'},
+        population:{$first:'$population'}
+    }},
+    {$sort: {_id:1}}
+])
